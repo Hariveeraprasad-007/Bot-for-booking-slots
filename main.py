14ff22b804f11e5c9e48ee88884724cd55acd1a0
Arikatla Hari Veera Prasad <arikatlahariveeraprasad007@gmail.com>
2025-05-02 05:12:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 import time
 driver=webdriver.Firefox()
 driver.get("https://lms2.ai.saveetha.in/course/view.php?id=302")
